a6b1889c4ced7b8029267c7b8146be0fad874053
@r0f33 <jiske.nicolson@gmail.com>
2024-03-28 22:17:08 +1030

    finished page functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,9 @@
 var key = "c4cf4b862a7cbf3d8ab3425135711b32";
-// let weatherIconUrl = `https://openweathermap.org/img/wn/${currentWeather.weather.icon}@2x.png`;
-let dateNow = dayjs();
-// console.log(dateNow.format('DD/MM/YYYY'));
-// console.log(`${dateNow.format('DD/MM/YYYY')}`);
 
 function renderSavedLocations() {
-
-    // answer not yet looked at
     let locations = JSON.parse(localStorage.getItem('savedLocations'));
-    // let locations = JSON.stringify(locationsObject);
-
-
-    // let locations = JSON.parse(localStorage.getItem('savedLocations'));
-    // console.log(locationsObject);
-    // console.log(locations);
 
     const pastSearches = document.querySelector('#pastsearches');
-    // const createLocationButton = document.createElement('button');
 
     if (!locations) {
         console.log('There are no saved locations');
@@ -33,6 +20,7 @@ function renderSavedLocations() {
 
 function renderDisplayedLocation() {
     let location = JSON.parse(localStorage.getItem('displayedLocation'));
+    let dateNow = dayjs();
 
     document.querySelector('#forecasth4').style.display = "block";
 
@@ -40,80 +28,76 @@ function renderDisplayedLocation() {
     forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&APPID=${key}&units=metric`
 
     fetch(currentWeatherUrl)
-    .then(function(response) {
-        if (response.status !== 200) {
-            throw currentWeatherUrl;
-        } else {
-            // console.log(response.name);
-            return response.json();
-        };
-    })
-    .then(function(currentWeather) {
-        if (!currentWeather) {
-            console.log('No results found!');
-        } else {
-            let weatherIconUrl = `https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`;
+        .then(function (response) {
+            if (response.status !== 200) {
+                throw currentWeatherUrl;
+            } else {
+                return response.json();
+            };
+        })
+        .then(function (currentWeather) {
+            if (!currentWeather) {
+                console.log('No results found!');
+            } else {
+                let weatherIconUrl = `https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`;
 
-            const container = document.getElementById('container');
+                const container = document.getElementById('container');
 
-            const weatherDetails = ['First paragraph', 'Second paragraph', 'Third paragraph'];
-
-            weatherDetails.forEach(weatherDetails => {
-                const pElement = document.createElement('p');
-                pElement.textContent = text;
-                container.appendChild(pElement);
-            });
-
-            // const createArticle = document.createElement('article');
-            const createH3 = document.createElement('h3');
-            const createImg = document.createElement('img');
-            createImg.src = weatherIconUrl;
-            const createP = document.createElement('p');
-
-            document.querySelector('#current').innerHTML = '';
-            document.querySelector('#current').append(createH3, createImg, createP);
-
-            createH3.textContent = `${currentWeather.name} (${dateNow.format('DD/MM/YYYY')})`;
-            createP.textContent = `Temp: ${currentWeather.main.temp} °C \nWind: ${currentWeather.wind.speed} km/h \nHumidity: ${currentWeather.main.humidity} %`        
-        }
-    })
-
-    fetch(forecastWeatherUrl)
-    .then(function(response) {
-        if (response.status !== 200) {
-            throw forecastWeatherUrl;
-        } else {
-            // console.log(response.name);
-            return response.json();
-        };
-    })
-    .then(function(forecastWeather) {
-        if (!forecastWeather) {
-            console.log('No results found!');
-        } else {
-            document.querySelector('#forecast').innerHTML = '';
-
-            for (let index = 0; index < 5; index++) {
-                const forecast = (forecastWeather.list)[index];
-                // console.log(forecast.wind.speed)
-                // console.log(forecast.weather[index].icon)
-                let weatherIconUrl = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
-                // console.log(forecast.wind.speed)
-
-                const createAside = document.createElement('aside');
+                const createH3 = document.createElement('h3');
                 const createImg = document.createElement('img');
                 createImg.src = weatherIconUrl;
-                const createH5 = document.createElement('h5');
-                const createP = document.createElement('p');
+                const createPTemp = document.createElement('p');
+                const createPWind = document.createElement('p');
+                const createPHumidity = document.createElement('p');
 
-                document.querySelector('#forecast').append(createAside);
-                createAside.append(createH5, createImg, createP);
+                createH3.textContent = `${currentWeather.name} (${dateNow.format('DD/MM/YYYY')})`;
+                createPTemp.textContent = `Temp: ${currentWeather.main.temp} °C`
+                createPWind.textContent = `Wind: ${currentWeather.wind.speed} km/h`
+                createPHumidity.textContent = `Humidity: ${currentWeather.main.humidity} %`
 
-                createH5.textContent = `${dateNow.add([index + 1], 'day').format('DD/MM/YYYY')}`;
-                createP.textContent = `Temp: ${forecast.main.temp} °C Wind: ${forecast.wind.speed} km/h Humidity: ${forecast.main.humidity} %`
+                document.querySelector('#current').innerHTML = '';
+                createH3.append(createImg);
+                document.querySelector('#current').append(createH3, createPTemp, createPWind, createPHumidity);
             }
-        }
-    })
+        })
+
+    fetch(forecastWeatherUrl)
+        .then(function (response) {
+            if (response.status !== 200) {
+                throw forecastWeatherUrl;
+            } else {
+                return response.json();
+            };
+        })
+        .then(function (forecastWeather) {
+            if (!forecastWeather) {
+                console.log('No results found!');
+            } else {
+                document.querySelector('#forecast').innerHTML = '';
+
+                for (let index = 0; index < 5; index++) {
+                    const forecast = (forecastWeather.list)[index];
+
+                    let weatherIconUrl = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
+
+                    const createAside = document.createElement('aside');
+                    const createImg = document.createElement('img');
+                    createImg.src = weatherIconUrl;
+                    const createH5 = document.createElement('h5');
+                    const createPTemp = document.createElement('p');
+                    const createPWind = document.createElement('p');
+                    const createPHumidity = document.createElement('p');
+
+                    document.querySelector('#forecast').append(createAside);
+                    createAside.append(createH5, createImg, createPTemp, createPWind, createPHumidity);
+
+                    createH5.textContent = `${dateNow.add([index + 1], 'day').format('DD/MM/YYYY')}`;
+                    createPTemp.textContent = `Temp: ${forecast.main.temp} °C`
+                    createPWind.textContent = `Wind: ${forecast.wind.speed} km/h`
+                    createPHumidity.textContent = `Humidity: ${forecast.main.humidity} %`
+                }
+            }
+        })
 };
 
 document.querySelector('#form').addEventListener('submit', function (inputLocation) {
@@ -128,36 +112,37 @@ document.querySelector('#form').addEventListener('submit', function (inputLocati
     };
 
     currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${key}&units=metric`
-    // forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&APPID=${key}`
-    
+
     fetch(currentWeatherUrl)
-    .then(function(response) {
-        if (response.status !== 200) {
-            alert('Please Enter Valid Australian Location');
-            throw currentWeatherUrl;
-        } else {
-            return response.json();
-        };
-    })
-    .then(function(currentWeather) {
-        console.log(currentWeather);
-        if (!currentWeather) {
-            console.log('No results found!');
-          } else {
-            locations.push(currentWeather.name);
+        .then(function (response) {
+            if (response.status !== 200) {
+                alert('Please Enter Valid Location');
+                throw currentWeatherUrl;
+            } else {
+                return response.json();
+            };
+        })
+        .then(function (currentWeather) {
+            console.log(currentWeather);
+            if (!currentWeather) {
+                console.log('No results found!');
+            } else {
+                locations.push(currentWeather.name);
 
-            localStorage.setItem('displayedLocation', JSON.stringify(currentWeather.name));
-            localStorage.setItem('savedLocations', JSON.stringify(locations));
+                localStorage.setItem('displayedLocation', JSON.stringify(currentWeather.name));
+                localStorage.setItem('savedLocations', JSON.stringify(locations));
 
-            renderDisplayedLocation();
-            renderSavedLocations();
-          }
-    })
+                document.querySelector('#location').value = '';
+
+                renderDisplayedLocation();
+                renderSavedLocations();
+            }
+        })
 });
 
 document.querySelector('#pastsearches').addEventListener('click', function (event) {
     localStorage.setItem('displayedLocation', JSON.stringify(event.target.textContent));
-    
+
     renderDisplayedLocation();
 });
 
